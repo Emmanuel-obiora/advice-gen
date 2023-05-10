bc001ee8fd85b15006d570fbff1258a4cd91dfb8
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-05-10 22:44:59 +0100

    Adding loading screen for API request

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,35 +1,52 @@
 import React, {useState, useEffect} from 'react';
 import IconDice from '../images/icon-dice.svg';
+import RingLoader from "react-spinners/RingLoader";
 
 const Main = () => {
     const [data, setData] = useState('');
+    const [loading, setLoading] = useState(true);
 
     const advice = () => {
+        setLoading(true);
         fetch('https://api.adviceslip.com/advice').then((data) => {
             // console.log(data);
             return data.json();
         }).then((completedata) => {
             // console.log(completedata);
             setData(completedata.slip);
+            setLoading(false);
         }).catch((err) =>{
             console.log(err);
+            setLoading(false);
         });
     }
 
     useEffect(()=> {
-        advice();
+        const interval = setInterval(()=>{
+            advice();
+        }, 8000);
+        return () => clearInterval(interval);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
   return (
     <main className='main'>
-        <div key={data.id} className="main-top">
-            <div className="main-top-hh">
-                <small>Advice</small>
-                <span>#{data.id}?</span>
-            </div>
-            <h1>"{data.advice}"</h1>
-        </div>
+        {
+            loading? 
+                <RingLoader
+                    color={"hsl(150, 100%, 66%)"}
+                    loading={loading}
+                    size={50}
+                    className='loaderr' 
+                /> :
+                <div key={data.id} className="main-top">
+                    <div className="main-top-hh">
+                        <small>Advice</small>
+                        <span>#{data.id}</span>
+                    </div>
+                    <h1>"{data.advice}"</h1>
+                </div>
+        }
 
         <span className='main-pattern'></span>
         <div className="main-bottom" onClick={advice}>
